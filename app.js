21f3f45018c323ffc573deae3c4b0bd5e475c5d6
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-21 17:13:53 +0000

    set username

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ This will be deemed as complete when the users cannot enter a username longer th
 🌟 BONUS: Make it so that valid usernames should only start with letters, not numbers or symbols.
 🌟 EXTRA BONUS: Make it so that the first letter of the username should be capitalised.
 */
+let isStarted = false;
 let userScore = 0;
 let compScore = 0;
-const username = document.querySelector('#username');
-const startGame = document.querySelector('.name');
-username.addEventListener('click', () => {
-	let;
-});
+// select + display user and comp scores in a span within the cards
+const player = document.querySelector('h2');
+const showUserScore = document.querySelector('#score-a');
+const showCompScore = document.querySelector('#score-b');
+const username = document.querySelector('input');
+const startGame = document.querySelector('.start');
+
+// validate username
+const setUserName = () => {
+	if (!player.textContent.match(/^[A-Z][a-z]{1,9}$/)) {
+		alert(
+			'username must start with a capital letter and be maximum 10 letters long'
+		);
+		player.textContent = `User`;
+	} else {
+		player.textContent = username.value;
+	}
+};
+
+startGame.addEventListener('click', setUserName);
 
 // Get the game choices buttons and the result element
 const rockBtn = document.querySelector('.rock');
@@ -33,7 +49,8 @@ function playGame(userChoice) {
 	const choices = ['rock', 'paper', 'scissors'];
 	const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-	// Determine the winner
+	// Determine the winner of each round
+	// Update user and computer scores, show them in the cards
 	let result;
 	if (
 		(userChoice === 'rock' && computerChoice === 'scissors') ||
@@ -41,12 +58,18 @@ function playGame(userChoice) {
 		(userChoice === 'scissors' && computerChoice === 'paper')
 	) {
 		userScore += 1;
-		result = `You Win! Your Score: ${userScore} Computer Score: ${compScore}`;
+		result = `You Win!`;
+		showUserScore.textContent = `${userScore}`;
+		showCompScore.textContent = `${compScore}`;
 	} else if (userChoice === computerChoice) {
-		result = `Tie! Your Score: ${userScore} Computer Score: ${compScore}`;
+		result = `Tie!`;
+		showUserScore.textContent = `${userScore}`;
+		showCompScore.textContent = `${compScore}`;
 	} else {
 		compScore += 1;
-		result = `Computer Wins! Your Score: ${userScore} Computer Score: ${compScore}`;
+		result = `Computer Wins!`;
+		showUserScore.textContent = `${userScore}`;
+		showCompScore.textContent = `${compScore}`;
 	}
 
 	// Show the result and play again button
@@ -62,4 +85,7 @@ playAgainBtn.addEventListener('click', () => {
 	resultEl.parentElement.style.display = 'none';
 	userScore = 0;
 	compScore = 0;
+	showUserScore.textContent = `${userScore}`;
+	showCompScore.textContent = `${compScore}`;
+	player.textContent = `User`;
 });
